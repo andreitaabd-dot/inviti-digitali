460f156be9e4a8d1b9b7634bba76dc58f1a3d5fc
andreitaabd-dot <andreita.abd@gmail.com>
2026-04-19 21:56:33 +0200

    Add files via upload

---
+++ inviti/raul_87/config.js
@@ -0,0 +1,48 @@
+const INVITO_CONFIG = {
+  nomeFesteggiato: "Raul",
+  eta: "86 anni",
+
+  titolo: "Compleanno di Raul",
+  sottotitolo: "Sei invitato a un pranzo speciale!",
+
+  descrizione1: "Ti aspettiamo per festeggiare insieme",
+  descrizione2: "gli 87 anni di Raul",
+
+  mese: "Aprile",
+  giorno: "29",
+  anno: "2026",
+  giornoSettimana: "Mercoledì",
+  ora: "13:00",
+
+  testoFinale: "Sarà un momento speciale da condividere insieme!",
+
+  whatsappNumero: "393277849274",
+  whatsappMessaggio: "Ciao! Confermo la presenza al compleanno di Raul",
+  indirizzoMaps: "Via Mombarcaro 49 Torino",
+
+  immagini: {
+    copertina: "dino.png",
+    sfondo: "bg-dino.jpg",
+    musica: "music.mp3"
+  },
+
+  effetti: ["sparkles"],
+
+  testoBottoneApri: "Apri l’invito",
+
+  tema: {
+    colorePrimario: "#8b5e3c",
+    coloreSecondario: "#f5e1c8",
+    coloreTesto: "#3e2c1c",
+    fontTitolo: "Georgia, serif",
+    fontTesto: "Georgia, serif"
+  },
+
+  envelope: "envelope.png",
+
+  introVideo: "intro.mp4",
+
+  decorazioneTop: {
+   
+  }
+};
