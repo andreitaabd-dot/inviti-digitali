995d503765424aae6b9e56993b9250327b36ebed
andreitaabd-dot <andreita.abd@gmail.com>
2026-04-20 23:05:08 +0200

    Add files via upload

---
+++ Ludovica_18/config.js
@@ -0,0 +1,52 @@
+const INVITO_CONFIG = {
+  nomeFesteggiato: "Ludovica Gianfreda",
+  eta: "18 anni",
+
+  titolo: "Compleanno di Ludovica",
+  sottotitolo: "Sei invitato a una festa speciale",
+
+  descrizione1: "Ti aspettiamo per festeggiare insieme",
+  descrizione2: "i 18 anni di Ludovica",
+
+  mese: "Dicembre",
+  giorno: "5",
+  anno: "2026",
+  giornoSettimana: "Sabato",
+  ora: "14:00",
+
+  testoFinale: "Ti aspetto per creare ricordi indimenticabili.\nAttendo con piacere una tua conferma.",
+
+  whatsappNumero: "4917671628584",
+  whatsappMessaggio: "Ciao! Confermo la mia presenza",
+
+  indirizzoMaps: "Ristorante & Pizzeria Ai Sapori, Schönborn-Allee 5, Bad Schönborn",
+
+  immagini: {
+    copertina: "dino.png",
+    sfondo: "bg-dino.jpg",
+    musica: "music.mp3"
+  },
+
+  effetti: ["sparkles"],
+
+  testoBottoneApri: "Apri invito",
+
+  tema: {
+    colorePrimario: "#0f3d3e",   // verde petrolio
+    coloreSecondario: "#d4af37", // oro
+    coloreTesto: "#2c2c2c",
+    fontTitolo: "Playfair Display, serif",
+    fontTesto: "Poppins, sans-serif"
+  },
+
+  envelope: "envelope.png",
+
+  introVideo: "intro.mp4",
+
+
+   dressCodeImage: "dresscode.jpeg",
+
+   decorazioneTop: {
+   
+  }
+};
