570e29a2a1bb9f8120dbde370aa293990998b341
andreitaabd-dot <andreita.abd@gmail.com>
2026-04-24 12:16:03 +0200

    Add files via upload

---
+++ inviti/bimbo_o_bimba/config.js
@@ -0,0 +1,51 @@
+const INVITO_CONFIG = {
+  nomeFesteggiato: "Famiglia Revellino",
+  eta: "",
+
+  titolo: "Maschietto o Femmina?",
+  sottotitolo: "Un momento speciale da vivere insieme",
+
+  descrizione1: "Unisciti a noi per scoprire",
+  descrizione2: "chi sta per arrivare",
+
+  mese: "Luglio",
+  giorno: "25",
+  anno: "2026",
+  giornoSettimana: "Sabato",
+  ora: "16:00",
+
+  testoFinale: "Preparati a una sorpresa indimenticabile!",
+
+  whatsappNumero: "393881769329",
+  whatsappMessaggio: "Ciao! Confermo la mia presenza alla rivelazione",
+  indirizzoMaps: "Corso IV Novembre, 10134 Torino TO",
+
+  immagini: {
+    copertina: "dino.png", // poi lo cambiamo
+    sfondo: "bg-dino.jpg", // poi lo cambiamo
+    musica: "music.mp3"
+  },
+
+  effetti: ["sparkles", "confetti"],
+
+  testoBottoneApri: "Scopri la sorpresa",
+
+  tema: {
+    colorePrimario: "#ff6ec7",   // rosa
+    coloreSecondario: "#6ecbff", // blu
+    coloreTesto: "#2c2c2c",
+    fontTitolo: "Georgia, serif",
+    fontTesto: "Georgia, serif"
+  },
+
+  envelope: "envelope.png",
+
+  introVideo: "intro.mp4",
+
+  decorazioneTop: {
+    immagine: "dino.png", // poi lo cambiamo con i bimbi
+    left: "50%",
+    top: "-180px",
+    width: "140px"
+  }
+};
