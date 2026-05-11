075303d9cae906f06152204a5cf56536be8cd62b
andreitaabd-dot <andreita.abd@gmail.com>
2026-05-11 09:17:59 +0200

    Add files via upload

---
+++ inviti/aureliano_safethedate/config.js
@@ -0,0 +1,50 @@
+const INVITO_CONFIG = {
+  nomeFesteggiato: "Aureliano",
+  eta: "3 anni",
+
+  titolo: "Aureliano",
+  sottotitolo: "Aureliano ti aspetta per la sua festa",
+
+  // descrizione1: "",
+  descrizione2: "Manca pochissimo!",
+
+  mese: "Giugno",
+  giorno: "14",
+  anno: "2026",
+  giornoSettimana: "Domenica",
+  ora: "10:00",
+
+  testoFinale: "Non vediamo l’ora di festeggiare insieme a voi!",
+
+  whatsappNumero: "393284322709",
+  whatsappMessaggio: "Ciao! Confermo la presenza alla festa di Aureliano",
+
+  indirizzoMaps: "Baby Gym - Via Vespucci 36 Torino",
+
+  immagini: {
+    copertina: "dino.png",
+    sfondo: "bg-dino.jpg",
+    sfondoInvito: "bg-invite.jpg",
+    musica: "music.mp3"
+  },
+
+  effetti: [],
+
+  testoBottoneApri: "Apri invito",
+
+ tema: {
+  colorePrimario: "#d89aa6",
+  coloreSecondario: "#d4af37",
+  coloreTesto: "#5c3b3f",
+  fontTitolo: "'Cormorant Garamond', serif",
+  fontTesto: "'Cormorant Garamond', serif",
+},
+
+  envelope: "envelope.png",
+
+
+
+    decorazioneTop: {
+   
+  }
+};
