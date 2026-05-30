6d5a5551526edf6b0442b867a34daf7f481b59af
andreitaabd-dot <andreita.abd@gmail.com>
2026-05-30 12:39:27 +0200

    Add files via upload

---
+++ inviti/aurora_18/config.js
@@ -0,0 +1,60 @@
+const INVITO_CONFIG = {
+  nomeFesteggiato: "Aurora",
+  eta: "18 anni",
+
+  titolo: "Aurora",
+  sottotitolo: "Sei invitato a una festa speciale",
+
+  descrizione1: "Ti aspetto per festegiare insieme",
+  descrizione2: "18 anni di Aurora",
+
+  mese: "Luglio",
+  giorno: "11",
+  anno: "2026",
+  giornoSettimana: "Sabato",
+  ora: "20:00",
+
+  testoFinale: "Ti aspetto per creare ricordi indimenticabili.\nè gradita la conferma entro il 25 giugno.",
+
+  whatsappNumero: "393313518192",
+  whatsappMessaggio: "Ciao! Confermo la mia presenza ai 18 anni di Aurora",
+
+  indirizzoMaps: "Via Macchie Rio, 65, 86029 Trivento CB",
+
+  linkFoto: "https://photos.app.goo.gl/bTGJKWv9GktepqZc7",
+
+  immagini: {
+    copertina: "dino.png",
+    sfondo: "bg-dino.jpg",
+    sfondoInvito: "bg-invite.jpg",
+    musica: "music.mp3"
+  },
+
+  effetti: [],
+
+  testoBottoneApri: "Apri invito",
+
+tema: {
+  colorePrimario: "#d4af37",   // oro
+  coloreSecondario: "#f7e7a9", // oro chiaro luminoso
+  coloreTesto: "#6b4b1f",      // marrone oro
+  fontTitolo: "'Cormorant Garamond', serif",
+  fontTesto: "'Cormorant Garamond', serif"
+},
+
+  envelope: "envelope.png",
+
+  effetti: ["sparkles"],
+/*
+  introVideo: "intro.mp4",
+
+  dressCodeImage: "dresscode.jpeg",
+
+*/
+   decorazioneTop: {
+  immagine: "dino.png",
+  width: "150px",
+  distanzaDaBusta: "-5px",
+}
+
+};
