bbf8211572c98ab5bfb3515d66e2654aa92ae6c6
andreitaabd-dot <andreita.abd@gmail.com>
2026-06-08 22:22:39 +0200

    Add files via upload

---
+++ inviti/despedida_soltera/config.js
@@ -0,0 +1,60 @@
+const INVITO_CONFIG = {
+  /*nomeFesteggiato: "Aurora",
+  eta: "18 anni",*/
+
+  titolo: "Despedida de Soltera",
+sottotitolo: "¡La última noche de libertad!",
+
+descrizione1: "Prepárate para una noche llena de risas, sorpresas y diversión",
+descrizione2: "Antes del gran día",
+
+    mese: "Settembre",
+giorno: "19",
+anno: "2026",
+giornoSettimana: "Sabato",
+ora: "21:00",
+
+
+ indirizzoMaps: "NH Collection Torino Piazza Carlina, Piazza Carlo Emanuele II 15, Torino",
+
+testoFinale: "Prepárate para una noche llena de risas, sorpresas y momentos inolvidables. ¡No puedes faltar!",
+
+whatsappNumero: "393881769329",
+whatsappMessaggio: "¡Hola! Confirmo mi asistencia a la despedida de soltera 💃🥂",
+
+  linkFoto: "https://photos.app.goo.gl/kLMpeTQW4Wg7y646A",
+
+  immagini: {
+    copertina: "",
+    sfondo: "bg-dino.jpg",
+    sfondoInvito: "bg-invite.jpg",
+    musica: "music.mp3"
+  },
+
+ effetti: ["sparkles"],
+
+  testoBottoneApri: "Apri invito",
+
+tema: {
+  colorePrimario: "#d4af37",
+  coloreSecondario: "#f7e7a9",
+  coloreTesto: "#f8f4e8",
+  fontTitolo: "'Cormorant Garamond', serif",
+  fontTesto: "'Cormorant Garamond', serif"
+},
+
+  envelope: "envelope.png",
+
+   dressCodeImage: "dresscode.jpeg",
+  effetti: ["sparkles"]
+/*
+  introVideo: "intro.mp4",
+
+   decorazioneTop: {
+  immagine: "dino.png",
+  width: "150px",
+  distanzaDaBusta: "-5px",
+}
+  */
+
+};
