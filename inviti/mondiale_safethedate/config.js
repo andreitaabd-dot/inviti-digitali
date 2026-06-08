267fd036a1ff8911334ebd633fd1bb97d47f949e
andreitaabd-dot <andreita.abd@gmail.com>
2026-06-08 21:29:58 +0200

    Add files via upload

---
+++ inviti/mondiale_safethedate/config.js
@@ -0,0 +1,52 @@
+const INVITO_CONFIG = {
+ nomeFesteggiato: "Mondiale 2026",
+
+  titolo: "Save the Date",
+  sottotitolo: "Il Mondiale sta arrivando!",
+
+  descrizione1: "Preparati a vivere emozioni uniche",
+  descrizione2: "Manca pochissimo!",
+
+  mese: "Giugno",
+  giorno: "11",
+  anno: "2026",
+  giornoSettimana: "Giovedì",
+  ora: "21:00",
+
+  testoFinale: "Il conto alla rovescia è ufficialmente iniziato!",
+
+  whatsappNumero: "393881769329",
+  whatsappMessaggio: "Presente! ⚽🏆 Il Mondiale non si guarda da soli... ci vediamo per tifare insieme! 🔥",
+
+  indirizzoMaps: "Via della Vittoria 26, Torino",
+
+  immagini: {
+    copertina: "dino.png",
+    sfondo: "bg-dino.jpg",
+    sfondoInvito: "bg-invite.jpg",
+    musica: "music.mp3"
+  },
+
+    effetti: ["sparkles", "confetti"],
+
+  testoBottoneApri: "Apri invito",
+
+tema: {
+  colorePrimario: "#0f5132",      // verde prato
+  coloreSecondario: "#d4af37",    // oro
+  coloreTesto: "#ffffff",
+
+  
+  fontTitolo: "'Cinzel', serif",
+  fontTesto: "'Cormorant Garamond', serif",
+},
+
+  envelope: "envelope.png",
+
+
+    decorazioneTop: {
+      immagine: "dino.png",
+      width: "130px",
+      distanzaDaBusta: "60px",
+    }
+};
